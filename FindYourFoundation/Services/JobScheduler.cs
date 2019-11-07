854b61d47ed45552ae7fdb1eb5eeaa9c00529045
LU <monkey07233@gmail.com>
2019-11-07 21:39:19 +0800

    新增 Quartz套件

---
+++ FindYourFoundation/Services/JobScheduler.cs
@@ -0,0 +1,21 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace FindYourFoundation.Services
+{
+    public class JobScheduler
+    {
+        public static void Start(bool toggle)
+        {
+            if (toggle)
+            {
+                IScheduler scheduler = new StdSchedulerFactory().GetScheduler();
+            }
+        }
+    }
+}
