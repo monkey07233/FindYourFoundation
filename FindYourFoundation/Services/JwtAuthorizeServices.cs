13f74fddbc326027466441c78f6aeff11a2e1c40
LU <monkey07233@gmail.com>
2019-11-07 21:43:13 +0800

    token時間改回一小時

--- FindYourFoundation/Services/JwtAuthorizeServices.cs
+++ FindYourFoundation/Services/JwtAuthorizeServices.cs
@@ -14,8 +14,7 @@ namespace FindYourFoundation.Services
             string secret = "FindYourFoundation";
             Dictionary<string, object> user = new Dictionary<string, object>();
             user.Add("Account", customer.Account);
-            user.Add("Exp", DateTime.Now.AddMinutes(3).ToString());
-            //user.Add("Exp", DateTime.Now.AddHours(1).ToString());
+            user.Add("Exp", DateTime.Now.AddHours(1).ToString());
             var payload = user;
             var token = Jose.JWT.Encode(payload, Encoding.UTF8.GetBytes(secret), Jose.JwsAlgorithm.HS512);
             return token;
