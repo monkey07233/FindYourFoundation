be0feb845b4d1c26ef66a0ef556fe521d4bee2cb
LU <monkey07233@gmail.com>
2019-11-11 10:49:04 +0800

    購物車預設數量為1

--- FindYourFoundation/Controllers/CartController.cs
+++ FindYourFoundation/Controllers/CartController.cs
@@ -26,6 +26,7 @@ namespace FindYourFoundation.Controllers
             {
                 foreach (var c in cart)
                 {
+                    c.Quantity = 1;
                     c.Url = "/ProductPic/" + Path.GetFileNameWithoutExtension(c.Url) + Path.GetExtension(c.Url);
                 }
                 return cart;
