1173e559e0586841de223c91867b385fe156576d
LU <monkey07233@gmail.com>
2019-11-16 14:44:10 +0800

    修正購買紀錄bug

--- FindYourFoundation/Repositoires/BuyHistoryRepo.cs
+++ FindYourFoundation/Repositoires/BuyHistoryRepo.cs
@@ -13,9 +13,9 @@ namespace FindYourFoundation.Repositoires
         {
             return Query<BuyHistoryViewModel>("select a.BuyHistory_Id,a.Product_Id,b.Brand,b.Name,a.Price,a.Quantity,a.BuyTime from BuyHistory as a,Product as b where a.Product_Id = b.Product_Id and a.Account = @Account", new { Account }).ToList();
         }
-        public int GetBuyTimes(string Account)
+        public List<string> GetBuyTimes(string Account)
         {
-            return Query<int>("select count(BuyHistory_Id) as times from BuyHistory where Account = @Account", new { Account }).FirstOrDefault();
+            return Query<string>("select convert(varchar,BuyTime,111) as times from BuyHistory where Account=@Account group by convert(varchar,BuyTime,111)", new { Account }).ToList();
         }
     }
 }
