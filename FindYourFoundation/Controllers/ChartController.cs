cc12396c6d3d7069e10cd6b0a80224997c348948
LU <monkey07233@gmail.com>
2019-11-18 20:43:18 +0800

    年齡區間長條圖

--- FindYourFoundation/Controllers/ChartController.cs
+++ FindYourFoundation/Controllers/ChartController.cs
@@ -14,14 +14,14 @@ namespace FindYourFoundation.Controllers
         private ChartRepo _chartRepo = new ChartRepo();
         // GET: Chart
         [HttpGet]
-        public List<BrandHistory> GetBrandHistory()
+        public List<Bar> GetBrandHistory()
         {
-            List<BrandHistory> brandHistories = new List<BrandHistory>();
+            List<Bar> brandHistories = new List<Bar>();
             var history = _chartRepo.GetBrandHistory();
-            var list= history.GroupBy(h => h.Brand).Select(g => new { Brand = g.Key, times = g.Sum(q => q.times) });
-            foreach(var b_history in list)
+            var list = history.GroupBy(h => h.Brand).Select(g => new { Brand = g.Key, times = g.Sum(q => q.times) });
+            foreach (var b_history in list)
             {
-                BrandHistory brandHistory = new BrandHistory();
+                Bar brandHistory = new Bar();
                 brandHistory.genre = b_history.Brand;
                 brandHistory.sold = b_history.times;
                 brandHistories.Add(brandHistory);
@@ -33,7 +33,44 @@ namespace FindYourFoundation.Controllers
         {
             return _chartRepo.GetGender();
         }
-        public class BrandHistory
+        [HttpGet]
+        public List<Bar> GetAge()
+        {
+            var age = _chartRepo.GetAge();
+            List<Bar> ageList = new List<Bar>();
+            for (int i = 0; i <= 4; i++)
+            {
+                Bar getAge = new Bar();
+                if (i == 0)
+                {
+                    getAge.sold = age.underTwenty;
+                    getAge.genre = "20歲以下";
+                }
+                if (i == 1)
+                {
+                    getAge.sold = age.twoTothree;
+                    getAge.genre = "21歲-30歲";
+                }
+                if (i == 2)
+                {
+                    getAge.sold = age.threeTofour;
+                    getAge.genre = "31歲-40歲";
+                }
+                if (i == 3)
+                {
+                    getAge.sold = age.fourTofive;
+                    getAge.genre = "41歲-50歲";
+                }
+                if (i == 4)
+                {
+                    getAge.sold = age.overFive;
+                    getAge.genre = "50歲以上";
+                }
+                ageList.Add(getAge);
+            }
+            return ageList;
+        }
+        public class Bar
         {
             public string genre { get; set; }
             public int sold { get; set; }
--- FindYourFoundation/Repositoires/ChartRepo.cs
+++ FindYourFoundation/Repositoires/ChartRepo.cs
@@ -19,6 +19,15 @@ namespace FindYourFoundation.Repositoires
         {
             return Query<GetGender>("select count(Gender) as value,Gender as label from Customer group by Gender").ToList();
         }
+        public GetAge GetAge()
+        {
+            return Query<GetAge>(@"select sum(case when year(getdate())-year(Birthday) <= 20 then 1 else 0 end) as underTwenty,
+                                    sum(case when year(getdate())-year(Birthday) between 21 and 30 then 1 else 0 end) as twoTothree,
+                                    sum(case when year(getdate())-year(Birthday) between 31 and 40 then 1 else 0 end) as threeTofour,
+                                    sum(case when year(getdate())-year(Birthday) between 41 and 50 then 1 else 0 end) as fourTofive,
+                                    sum(case when year(getdate())-year(Birthday) >50 then 1 else 0 end) as overFive
+                                    from Customer").FirstOrDefault();
+        }
     }
     public class GetBrandHistory
     {
@@ -29,5 +38,13 @@ namespace FindYourFoundation.Repositoires
     {
         public string label { get; set; }
         public int value { get; set; }
+    }
+    public class GetAge
+    {
+        public int underTwenty { get; set; }
+        public int twoTothree { get; set; }
+        public int threeTofour { get; set; }
+        public int fourTofive { get; set; }
+        public int overFive { get; set; }
     }
 }
